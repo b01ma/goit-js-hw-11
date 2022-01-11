13a3bf25c270646623feccebb14e0de50af6fd03
ed_bolma <ed.bolma21@gmail.com>
2022-01-11 17:34:04 +0100

    started. fetch is working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
+import axios from 'axios';
 import './sass/main.scss';
+
+const refs = {
+    button: document.querySelector('button'),
+    body: document.querySelector('.body-container'),
+}
+
+// console.log(refs.body);
+// refs.body.insertAdjacentHTML('beforeend', '<h1>hello world</h1>');
+
+const BASE_URL = 'https://pixabay.com/api/?key=14665608-5c267132ac5256a05f9292b82';
+const KEY = '14665608-5c267132ac5256a05f9292b82';
+
+refs.button.addEventListener('click', onClickEvent);
+
+function onClickEvent(event) {
+
+    event.preventDefault();
+
+    axios.get('https://pixabay.com/api/?key=14665608-5c267132ac5256a05f9292b82&q=yellow+flowers&image_type=photo')
+        .then(r => {
+            const dataArray = r.data.hits;
+            const imageArray = dataArray.map(data => data.webformatURL);
+
+            console.log(imageArray);
+
+            const renderImage = imageArray.map(url => {
+                refs.body.insertAdjacentHTML('beforeend', `<img src="${url}" alt="картинка">`);
+            })
+
+
+        })
+        .then(console.log);
+   
+
+  
+}
+
+
+
+
+  
+
