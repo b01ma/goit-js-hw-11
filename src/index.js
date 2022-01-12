616ca82ebef36afd5f13307fbfd20622bae7a0e8
ed_bolma <ed.bolma21@gmail.com>
2022-01-12 16:49:59 +0100

    js search engine is almost complete. needs few adjustments. Then will work on html and css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './sass/main.scss';
 const refs = {
     button: document.querySelector('button'),
     gallery: document.querySelector('.image-gallery'),
+    input: document.querySelector('input'),
 }
 
 // console.log(refs.body);
@@ -11,33 +12,81 @@ const refs = {
 
 const BASE_URL = 'https://pixabay.com/api/?key=14665608-5c267132ac5256a05f9292b82';
 const KEY = '14665608-5c267132ac5256a05f9292b82';
+let searchQueryText = ''.trim();
 
+refs.input.addEventListener('input', onInputEvent);
 refs.button.addEventListener('click', onClickEvent);
 
-function onClickEvent(event) {
-
-    event.preventDefault();
-
-    axios.get('https://pixabay.com/api/?key=14665608-5c267132ac5256a05f9292b82&q=yellow+flowers&image_type=photo')
-        .then(r => {
-            const dataArray = r.data.hits;
-            const imageArray = dataArray.map(data => data.webformatURL);
-
-            console.log(imageArray);
-
-            const renderImage = imageArray.map(url => {
-                refs.gallery.insertAdjacentHTML('beforeend', `<img src="${url}" alt="картинка">`);
-            })
-
-
-        })
-        .then(console.log);
-   
-
-  
+function onInputEvent(event) {
+    searchQueryText = event.currentTarget.value;
 }
 
+function onClickEvent(event) {
+    event.preventDefault();
 
+    clearGallery();
+
+    const options = {
+        imageType: "photo",
+        page: 1,
+        perPage: 10,
+    }
+
+    if (!searchQueryText) {
+        return;
+    };
+
+    fetchPicture(searchQueryText, options)
+        .then(r => {
+
+            if (!r.data.totalHits) {
+                alert('no resutls');
+            }
+
+            renderImage(r)
+        });
+  
+};
+
+function fetchPicture(query, options) {
+     return axios.get(`https://pixabay.com/api/?key=14665608-5c267132ac5256a05f9292b82&q=${query}&image_type=${options.imageType}&page=${options.page}&per_page=${options.perPage}`)
+}
+
+function renderImage(response) {
+    const dataArray = response.data.hits;
+            dataArray.map(data => { 
+
+                const url = data.webformatURL;
+                const likes = data.likes;
+                const views = data.views;
+                const comments = data.comments;
+                const downloads = data.downloads;
+
+
+                refs.gallery.insertAdjacentHTML('beforeend',
+                    `<div class="photo-card">
+                        <img src="${url}" />
+                        <div class="info">
+                            <p class="info-item">
+                            <b>Likes</b>${likes}
+                            </p>
+                            <p class="info-item">
+                            <b>Views</b> ${views}
+                            </p>
+                            <p class="info-item">
+                            <b>Comments</b>${comments}
+                            </p>
+                            <p class="info-item">
+                            <b>Downloads</b>${downloads}
+                            </p>
+                        </div>
+                    </div>`);
+            });
+};
+
+function clearGallery() {
+    refs.gallery.innerHTML = '';
+}
 
 
   
